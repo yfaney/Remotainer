9d70379e5e5a5d52ae26d3fd8d8249da9c23c69d
Jang,Younghwan <Younghwan.Jang@cerner.com>
2018-07-10 17:29:02 -0500

    IterWFD001 Changing comm method to WFD

--- app/src/main/java/com/yfaney/remotainer/MainActivity.java
+++ app/src/main/java/com/yfaney/remotainer/MainActivity.java
@@ -2,14 +2,19 @@ package com.yfaney.remotainer;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pGroup;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import com.yfaney.remotainer.util.BluetoothManager;
-import com.yfaney.remotainer.util.IBluetoothManager;
 import com.yfaney.remotainer.util.PreferenceManager;
 
 import java.util.HashSet;
@@ -19,20 +24,44 @@ public class MainActivity extends AppCompatActivity {
     public final static String KEY_CONTROLLER = "Controller";
     public final static String KEY_PLAYER = "Player";
     public final static String REMOTAINER_PLAYER_PREFIX = "FP-";
-    private static final int REQUEST_ENABLE_BLUETOOTH = 0x00000001;
+    private static final int REQUEST_ENABLE_WIFI = 0x00000001;
     private static final int REQUEST_DOPAIR_BLUETOOTH = 0x00000010;
+
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel mChannel;
+    private WifiP2pManager mManager;
+    private WiFiDirectBroadcastReceiver receiver;
+    private boolean isWifiP2pEnabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        receiver = new WiFiDirectBroadcastReceiver();
+        receiver.setActivity(this);
+        receiver.setWiFiDirectProperties(mManager, mChannel);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        checkBluetoothAvailability();
+        checkWiFiAvailability();
 
         String runningMode = PreferenceManager.getRunningMode();
         if(KEY_PLAYER.equals(runningMode)){
@@ -40,17 +69,30 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /** register the BroadcastReceiver with the intent values to be matched */
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
     /**
-     * Check Bluetooth Availability and open Bluetooth Setting if necessary.
+     * Check WiFi Availability and open Bluetooth Setting if necessary.
      */
-    private void checkBluetoothAvailability() {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled()) {
-            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
+    private void checkWiFiAvailability() {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager != null && !wifiManager.isWifiEnabled()) {
+            Intent enableIntent = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_WIFI);
         } else {
-            Toast.makeText(this, "Bluetooth is not available!", Toast.LENGTH_SHORT).show();
-            finish(); //automatic close app if Bluetooth service is not available!
+            Toast.makeText(this, "WiFi is not available!", Toast.LENGTH_SHORT).show();
+            finish(); //automatic close app if WiFi service is not available!
         }
     }
 
@@ -63,29 +105,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadRemotainer(String runningMode) {
-        boolean isSuccessfullyPaird = false;
-        final BluetoothManager bluetoothManager = BluetoothManager.getSingleton();
-        if (PreferenceManager.getPrePairdDevice() != null) {
-            isSuccessfullyPaird = bluetoothManager.isSuccessfullyPaired();
-        }
-        if (isSuccessfullyPaird) {
-            if (KEY_CONTROLLER.equals(runningMode)) {
-                //Load Controller Activity
-                Set<BluetoothDevice> pairedDevices = new HashSet<>();
-                for (BluetoothDevice device : bluetoothManager.getAvailablePeers()){
-                    String device_name = device.getName();
-                    if(device_name.startsWith(REMOTAINER_PLAYER_PREFIX)){
-                        pairedDevices.add(device);
+        if (KEY_CONTROLLER.equals(runningMode)) {
+            mManager.requestGroupInfo(mChannel, new WifiP2pManager.GroupInfoListener() {
+                @Override
+                public void onGroupInfoAvailable(WifiP2pGroup group) {
+                    if(!group.getNetworkName().startsWith(REMOTAINER_PLAYER_PREFIX)){
+                        return;
                     }
-                    loadControllerService(pairedDevices);
+                    final Set<WifiP2pDevice> pairedDevices = new HashSet<>();
+                    String groupPassword = group.getPassphrase();
+                    for(WifiP2pDevice device : group.getClientList()){
+                        String device_name = device.deviceName;
+                        if(device_name.startsWith(REMOTAINER_PLAYER_PREFIX)){
+                            pairedDevices.add(device);
+                        }
+                        loadControllerService(pairedDevices);
+                    }
                 }
-            } else if (KEY_PLAYER.equals(runningMode)) {
-                //Load Player Activity
-                loadPlayerService();
-            }
-        } else {
-            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
+            });
+        } else if (KEY_PLAYER.equals(runningMode)) {
+            //Load Player Activity
+            loadPlayerService();
         }
     }
 
@@ -96,11 +136,11 @@ public class MainActivity extends AppCompatActivity {
         //TODO Implement Calling Player Service
     }
 
-    private void loadControllerService(final Set<BluetoothDevice> pairedDevices) {
+    private void loadControllerService(final Set<WifiP2pDevice> pairedDevices) {
         //1. Save the current mode so that we can automatically run this mode next time
         PreferenceManager.setRunningMode(KEY_CONTROLLER);
         //2. Pass the paired BluetoothDevice instance
-        PreferenceManager.setPrePairdDevices(pairedDevices);
+        //PreferenceManager.setPrePairdDevices(pairedDevices);
         //3. Run Controller service
         //TODO Implement Calling Controller Service
     }
@@ -108,13 +148,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Check which request we're responding to
-        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+        if (requestCode == REQUEST_ENABLE_WIFI) {
             // Make sure the request was successful
             if (resultCode == RESULT_OK) {
                 //Now Bluetooth enabled. Do nothing.
             } else {
-                Toast.makeText(this, "Bluetooth needs to be enabled to use this app.", Toast.LENGTH_SHORT).show();
-                finish(); //automatic close app if Bluetooth service is not enabled!
+                Toast.makeText(this, "WiFi needs to be enabled to use this app.", Toast.LENGTH_SHORT).show();
+                finish(); //automatic close app if WiFi service is not enabled!
             }
         } else if (requestCode == REQUEST_DOPAIR_BLUETOOTH) {
             // Make sure the request was successful
@@ -122,10 +162,17 @@ public class MainActivity extends AppCompatActivity {
 
             } else {
                 Toast.makeText(this, "The device needs to be paired to use this app.", Toast.LENGTH_SHORT).show();
-                finish(); //automatic close app if Bluetooth service is not enabled!
+                finish(); //automatic close app if WiFi service is not enabled!
             }
 
         }
     }
 
+    public void setIsWifiP2pEnabled(final boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
+
+    public boolean getIsWifiP2pEnabled() {
+        return isWifiP2pEnabled;
+    }
 }
---
+++ app/src/main/java/com/yfaney/remotainer/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,54 @@
+package com.yfaney.remotainer;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+    private MainActivity activity;
+    private WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+
+    public void setActivity(final MainActivity activity) {
+        this.activity = activity;
+    }
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Determine if Wifi P2P mode is enabled or not, alert
+            // the Activity.
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                activity.setIsWifiP2pEnabled(true);
+            } else {
+                activity.setIsWifiP2pEnabled(false);
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            // The peer list has changed! We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+            // Connection state changed! We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+//            DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
+//                    .findFragmentById(R.id.frag_list);
+//            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
+//                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
+
+        }
+    }
+
+    public void setWiFiDirectProperties(final WifiP2pManager mManager, final WifiP2pManager.Channel mChannel) {
+        this.mManager = mManager;
+        this.mChannel = mChannel;
+    }
+}
